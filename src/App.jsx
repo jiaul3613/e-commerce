e7746d02f79d01ee3d8309306f80d87f19074e29
jiaul <jiaul3613@gmail.com>
2026-06-04 17:42:20 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 //import React from 'react';
 
-import FancyButton from "./FancyButton";
+import FancyButton from "./FancyButton.jsx";
+import Nav from "./Nav.jsx"
 
 // Destructuring 'text' directly from the incoming props object
 //import Message from "./Message";
@@ -10,14 +11,10 @@ import FancyButton from "./FancyButton";
 const App = () => {
   return (
     <div className="mx-auto items-center bg-gray-200">
-      {/* Passing the 'text' prop correctly */}
-      {/* <Message />
-      <Message />
-
-      <Message text="Hi I am Jiaul Hoque" /> */}
+      <Nav />
     <h1 className="text-center text-5xl py-10 text-red-800">Wellcome to React</h1>
 
-    <FancyButton text='jiaul' />
+    <FancyButton text='jiaul'  />
     <FancyButton text='click' />
     <FancyButton text='submit' />
     </div>
---
+++ src/Nav.jsx
@@ -0,0 +1,18 @@
+//import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="max-w-5xl mx-auto items-center">
+        <ul className="flex justify-between py-5 px-5 uppercase  ">
+            <li><a href="">Home</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">Product</a></li>
+            <li><a href="">Devices</a></li>
+            <li><a href="">Facilites</a></li>
+            <li><a href="">Media</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
